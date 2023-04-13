5227532ac5a467b30c0903422ee873799cf295d4
Yorkay11 <85652149+Yorkay11@users.noreply.github.com>
2023-04-13 21:48:13 +0000

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -28,7 +28,7 @@ export default function Footer() {
               mt: ['10px', null, null, null, null, '0'],
             }}
           >
-            Copyright by {new Date().getFullYear()} DevOne Consulting 2023
+            Copyright by {new Date().getFullYear()} DevOne Consulting
           </Text>
         </Box>
         <Box sx={styles.linksWrap}>
