0f8607cc377e77b5e82d7096273c1d63f771c8fb
Yorkay11 <85652149+Yorkay11@users.noreply.github.com>
2023-04-13 22:10:44 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,9 +5,4 @@ const nextConfiguration = {
   target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
 };
 
-// module.exports = withPlugins([optimizedImages], nextConfiguration);
-module.exports = {
-  devIndicators: {
-    autoPrerender: false,
-  },
-}
+module.exports = withPlugins([optimizedImages], nextConfiguration);
